db76de704dae82fbe4361eaffd59d6ae354bc6ed
hugodessomme <hdessomme@gmail.com>
2019-03-13 20:43:32 +0100

    Move configuration to config folder

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -3,7 +3,7 @@
 use Core\App;
 use Core\Database\{QueryBuilder, Connection};
 
-App::bind('config', require dirname(__DIR__).'/config.php');
+App::bind('config', require dirname(__DIR__).'/config/config.php');
 
 App::bind('database',  new QueryBuilder(
     Connection::make(App::get('config')['database'])
--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ use Core\{Router, Request};
 require dirname(__DIR__).'/vendor/autoload.php';
 require dirname(__DIR__).'/core/bootstrap.php';
 
-Router::load(dirname(__DIR__).'/app/routes.php')
+Router::load(dirname(__DIR__).'/config/routes.php')
     ->direct(Request::uri(), Request::method());
