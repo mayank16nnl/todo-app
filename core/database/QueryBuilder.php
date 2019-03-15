936b51727ebde508a94a9f3fc73a030ffc1ede70
hugodessomme <hdessomme@gmail.com>
2019-03-15 20:12:17 +0100

    Add insert method to QueryBuilder

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -5,18 +5,46 @@ namespace Core\Database;
 class QueryBuilder
 {
     protected $pdo;
+    protected $statement;
 
     public function __construct(\PDO $pdo)
     {
         $this->pdo = $pdo;
     }
 
-    public function selectAll($table)
+    /**
+     * Display all entries from a table
+     *
+     * @param string $table
+     * @return array
+     */
+    public function findAll($table, $orderBy = 'DESC')
     {
-        $statement = $this->pdo->prepare("SELECT * FROM {$table}");
+    $statement = $this->pdo->prepare("SELECT * FROM {$table} ORDER BY id {$orderBy}");
 
         $statement->execute();
 
-        return $statement->fetchAll(\PDO::FETCH_CLASS);
+        return $statement->fetchAll(\PDO::FETCH_CLASS, 'App\\Model\\'.ucfirst($table));
+    }
+
+    /**
+     * Insert an entry in a table
+     *
+     * @param string $table
+     * @param array $parameters
+     * @return boolean
+     */
+    public function create($table, $parameters = [])    
+    {
+        $statement = sprintf(
+            "INSERT INTO %s(%s) VALUES(%s)",
+            $table,
+            implode(', ', array_keys($parameters)),
+            ':'.implode(', :', array_keys($parameters))
+        );
+
+        $statement = $this->pdo->prepare($statement);
+
+        return $statement->execute($parameters);
     }
 }
