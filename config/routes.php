24fb9200143791467a1bf6aa3a8fd7c3a5e5115d
hugodessomme <hdessomme@gmail.com>
2019-03-24 18:28:07 +0100

    Add routes

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,12 @@
 <?php
 
+// Users
+$router->get('/login', 'UserController::login');
+$router->get('/profile', 'UserController::show');
+$router->get('/profile/edit', 'UserController::edit');
+$router->post('/profile/update', 'UserController::update');
+$router->post('/user/store', 'UserController::store');
+
 // Pages
 $router->get('/', 'PageController::home');
 $router->get('/about', 'PageController::about');
@@ -7,7 +14,7 @@ $router->get('/contact', 'PageController::contact');
 $router->post('/contact', 'PageController::contact');
 
 // Tasks
-$router->get('/task/new', 'TaskController::new');
+$router->get('/task/new', 'TaskController::create');
 $router->get('/task/:id', 'TaskController::show')
        ->with('id', '[0-9]+');
 $router->get('/task/:id/edit', 'TaskController::edit')
